f2c1f777b08955aec049aecda73f8113c09444b4
Tatsuki <yookskii@gmail.com>
2020-03-10 18:02:21 +0700

    1.00

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 struct Rect{
@@ -8,10 +9,33 @@ struct Rect{
 double overlap(Rect,Rect);
 
 int main(){
+	Rect R1,R2;
 	cout << "Please input Rect 1 (x y w h): ";
+	cin>>R1.x>>R1.y>>R1.w>>R1.h;
 	cout << "Please input Rect 2 (x y w h): ";
+	cin>>R2.x>>R2.y>>R2.w>>R2.h;
+	cout << "Overlap area = "<<overlap(R1,R2);
 	
-	
-	cout << "Overlap area = ";	
 	return 0;
+}
+double findarea(Rect R){
+	return R.w*R.h;
+}
+
+
+double overlap(Rect R1,Rect R2){
+	double a,b,c,d,e,f;
+	Rect R3;
+	a = findarea(R1);
+	b = findarea(R2);
+	//if(R1.x < (R2.x+R2.w) && R1.y > (R2.y-R2.h) && (R1.x+R1.w) > R2.x && (R1.y-R1.h) > R2.y){
+		R3.w = min(R1.x+R1.w,R2.x+R2.w)-max(R1.x,R2.x);
+		R3.h = min(R1.y,R2.y)-max(R1.y-R1.h,R2.y-R2.h);
+		e = findarea(R3);
+		if (e>=0){
+			return e;
+		}else return 0;
+	//}else return 0;
+
+	
 }
--- lab18_2.cpp
+++ lab18_2.cpp
@@ -1,1 +1,42 @@
 //Modify function overlap() form lab18_1.cpp by using pointers as input arguments.
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+struct Rect{
+	double x,y,w,h;
+};
+
+double overlap(Rect *,Rect *);
+
+int main(){
+	Rect R1,R2;
+    Rect *a =&R1;
+    Rect *b =&R2;
+	cout << "Please input Rect 1 (x y w h): ";
+	cin>>a->x>>a->y>>a->w>>a->h;
+	cout << "Please input Rect 2 (x y w h): ";
+	cin>>b->x>>b->y>>b->w>>b->h;
+	cout << "Overlap area = "<< overlap(&R1,&R2);
+	
+	return 0;
+}
+double findarea(Rect R){
+	return R.w*R.h;
+}
+
+
+double overlap(Rect *a,Rect *b){
+	Rect R3;
+    Rect *c = &R3;
+	double e;
+		c->w = min(a->x+a->w,b->x+b->w)-max(a->x,b->x);
+		c->h = min(a->y,b->y)-max(a->y-a->h,b->y-b->h);
+		e = findarea(R3);
+		if (e>=0){
+			return e;
+		}else return 0;
+	
+
+	
+}
